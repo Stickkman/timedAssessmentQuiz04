bc2f1ee78deac236b16a61bedbe8d2e46494a17e
stickkman <stickkman@gmail.com>
2022-12-04 21:59:56 -0500

    reworked entire js logic, solved issues with alert window repeats

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,247 @@ var introCard = document.querySelector(".introCard")
 var startButton = document.querySelector("#startButton");
 var timerHead = document.querySelector("#headerTimer");
 var timerCount = 120; // set initial timer to 2 minutes
-var questionCard = document.querySelector("#questionContainer") 
+var questionCard = document.querySelector("#questionContainer"); 
+// keeps track of correct answers
+var correctAnswers = 0;
+//keeps track of wrong answers
+var wrongAnswers = 0;
+
+var questionDisplay = document.querySelector("#question");
+var choiceDisplay = document.querySelector('#choiceButtons');
+
+// array of questions
+var questions = [ 
+    'What would happen if you forgot to include a link to your JavaScript file in your html index ?',
+    'What does CSS stand for?', 
+    'What code generates a button in html?',
+    'What does != mean in JavaScript', 
+    'Which one of these is NOT an HTML element' 
+]
+
+//array of possible answers
+var answerChoices =[
+    'Your computer would catch on fire',
+    'None of your JavaScript code will run',
+    'Your internet will stop working',
+    'Your JavaScript will work anyway',
+    'Computational Server Station', 
+    'Cascading Style Sheet',
+    'Computer Service System', 
+    'Common Science', 
+	'<h1>Make a button</h1>',
+	'<bold> insert button </bold>',
+	'<button>This is a button</button>',
+	'make button here',
+	'Restart your router',
+	'Computer is overheating',
+	'Your antivirus subscription is ending soon',
+	'Not equal to',
+	'<img>',
+	'<footer>',
+	'<h3>',
+	'They are all HTML elements'
+	
+]
+// populates question
+function renderQuestion() {
+    console.log("questions start call"); //test
+        
+    questionDisplay.innerText = questions[0];
+
+        {
+        b1.textContent = answerChoices[0]; 
+        b2.textContent = answerChoices[1];
+        b3.textContent = answerChoices[2];
+        b4.textContent = answerChoices[3];
+        
+        choiceDisplay.addEventListener("click", (e)=>{ ///this is WHERE I LEFT LEFT
+            e.stopPropagation;
+            var element = e.target; // assign var element to target value
+            console.log(element);  // test
+
+ 
+            if (element.value == "correct") //if correct answer chosen, alert user and add to correct answer total
+            {   
+                e.stopPropagation();alert("Correct Answer"); correctAnswers++;  element.setAttribute("value", "correct");renderQuestion2();
+            
+                
+            }
+            else if (element.value =="wrong"){ // if wrong answer, alert user and add to wrong answer total;
+                console.log("Wrong Answer Chosen");  //test
+                alert("Wrong Answer"); wrongAnswers++; element.setAttribute("value", "reset");renderQuestion2();
+ 
+            } return;
+            
+            
+          
+            
+            
+            
+		}, { once: true }); 
+    } 
+
+    function renderQuestion2() {
+    console.log("questions2 start call"); //test
+        
+    questionDisplay.innerText = questions[1];
+
+        {
+        b1.textContent = answerChoices[4]; 
+        b2.textContent = answerChoices[5];
+        b3.textContent = answerChoices[6];
+        b4.textContent = answerChoices[7];
+        
+        choiceDisplay.addEventListener("click", (e)=>{ 
+            e.stopPropagation;
+            var element = e.target; // assign var element to target value
+            console.log(element);  // test
+
+			
+            if (element.value == "correct") //if correct answer chosen, alert user and add to correct answer total
+            {   
+                e.stopPropagation();alert("Correct Answer"); correctAnswers++;  element.setAttribute("value2", "correct");renderQuestion3();
+            
+                
+            }
+            else { // if wrong answer, alert user and add to wrong answer total;
+                console.log("Wrong Answer Chosen");  //test
+                alert("Wrong Answer"); wrongAnswers++; element.setAttribute("value", "reset");renderQuestion3();
+ 
+            } return;
+            
+            
+          
+            
+            
+            
+		}, { once: true }); 
+    }  
+	}  
+	function renderQuestion3() {
+    console.log("questions3 start call"); //test
+        
+    questionDisplay.innerText = questions[2];
+
+        {
+        b1.textContent = answerChoices[8]; 
+        b2.textContent = answerChoices[9];
+        b3.textContent = answerChoices[10];
+        b4.textContent = answerChoices[11];
+        
+        choiceDisplay.addEventListener("click", (e)=>{ 
+            e.stopPropagation;
+            var element = e.target; // assign var element to target value
+            console.log(element);  // test
+
+			
+            if (element.textContent == "<button>This is a button</button>") //if correct answer chosen, alert user and add to correct answer total
+            {   
+                e.stopPropagation();alert("Correct Answer"); correctAnswers++;  element.setAttribute("value2", "reset");renderQuestion4();
+            
+                
+            }
+            else { // if wrong answer, alert user and add to wrong answer total;
+                console.log("Wrong Answer Chosen");  //test
+                alert("Wrong Answer"); wrongAnswers++; element.setAttribute("value", "reset");renderQuestion4();
+ 
+            } return;
+            
+            
+          
+            
+            
+            
+		}, { once: true }); 
+    }  
+	}  
+	
+	function renderQuestion4() {
+    console.log("questions4 start call"); //test
+        
+    questionDisplay.innerText = questions[3];
+
+        {
+        b1.textContent = answerChoices[12]; 
+        b2.textContent = answerChoices[13];
+        b3.textContent = answerChoices[14];
+        b4.textContent = answerChoices[15];
+        
+        choiceDisplay.addEventListener("click", (e)=>{ 
+            e.stopPropagation;
+            var element = e.target; // assign var element to target value
+            console.log(element);  // test
+
+			
+            if (element.textContent == "Not equal to") //if correct answer chosen, alert user and add to correct answer total
+            {   
+                e.stopPropagation();alert("Correct Answer"); correctAnswers++;  element.setAttribute("value2", "reset");renderQuestion5();
+            
+                
+            }
+            else { // if wrong answer, alert user and add to wrong answer total;
+                console.log("Wrong Answer Chosen");  //test
+                alert("Wrong Answer"); wrongAnswers++; element.setAttribute("value", "reset");renderQuestion5();
+ 
+            } return;
+            
+            
+          
+            
+            
+            
+		}, { once: true }); 
+    }  
+	} 
+	
+	function renderQuestion5() {
+    console.log("questions5 start call"); //test
+        
+    questionDisplay.innerText = questions[4];
+
+        {
+        b1.textContent = answerChoices[16]; 
+        b2.textContent = answerChoices[17];
+        b3.textContent = answerChoices[18];
+        b4.textContent = answerChoices[19];
+        
+        choiceDisplay.addEventListener("click", (e)=>{ 
+            e.stopPropagation;
+            var element = e.target; // assign var element to target value
+            console.log(element);  // test
+
+			
+            if (element.textContent == "They are all HTML elements") //if correct answer chosen, alert user and add to correct answer total
+            {   
+                e.stopPropagation();alert("Correct Answer"); correctAnswers++;  element.setAttribute("value2", "reset");finishQuiz();
+            
+                
+            }
+            else { // if wrong answer, alert user and add to wrong answer total;
+                console.log("Wrong Answer Chosen");  //test
+                alert("Wrong Answer"); wrongAnswers++; element.setAttribute("value", "reset");finishQuiz();
+ 
+            } return;
+            
+            
+          
+            
+            
+            
+		}, { once: true }); 
+    }  
+	} 
+       
+
+
+    // const b = document.createElement("button");
+    // b.innerHTML= answerChoices[0];
+    // choiceDisplay.appendChild(b);
+        // choiceDisplay.innerText = answerChoices[0];
+
+
+    
+}
 
 // startQuiz function
 function startQuiz() {
@@ -13,7 +253,7 @@ function startQuiz() {
     startButton.setAttribute("id", "hide"); //hides startbutton
     startButton.setAttribute("class", "hide"); //hides startbutton
     questionCard.setAttribute("class", "visible"); //reveals first set of questions
-    
+    renderQuestion();
 
 }
 
@@ -29,17 +269,12 @@ function startTimer(){
 
         }
             else {
-                timer.textContent = timerCount; questions();
+                timer.textContent = timerCount; 
             }
         
     },1000); // 1 second interval
 }
 
-function questions() {
-    console.log("questions start call");
-
-    
-}
 
 
 // call startQuiz funciton when start button is pushed
